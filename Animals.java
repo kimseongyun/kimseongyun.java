2a0033537cfe2adf880f6b77d6cd820786ec5b56
kimseongyun <fri1121@naver.com>
2016-05-23 21:26:08 +0900

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,17 @@
+class Animals {
+  public static void main(String[] args) {
+    // 
+    animal[] myArray;
+
+    Animal zoo = new Animal[3];
+    
+    
+    zoo[0] = new Animal["lion","roar"];
+    zoo[1] = new Animal["cat","meow"];
+    zoo[2] = new Animal["snake","hiss"];
+    
+    System.out.println("Element at index 0: " + anArray[0]);
+    System.out.println("Element at index 1: " + anArray[1]);
+    System.out.println("Element at index 2: " + anArray[2]);
+  }
+}
